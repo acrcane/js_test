bf27a8defcab8b6daf06e976f61bcbece1a8d6be
Enital <117684114+Enital@users.noreply.github.com>
2023-02-12 11:47:06 +0200

    task4

--- index.js
+++ index.js
@@ -27,25 +27,34 @@
 // }
 // console.log(total);
 
-function getTotal(max, min) {
-    let total = 0;
-    for (let i = max; i >= min; i -= 1) {
-        if (i % 2 !== 0) {
-            continue;
-        }
-        total += i;
-    }
-    return total;
-}
+// function getTotal(max, min) {
+//     let total = 0;
+//     for (let i = max; i >= min; i -= 1) {
+//         if (i % 2 !== 0) {
+//             continue;
+//         }
+//         total += i;
+//     }
+//     return total;
+// }
 
-console.log(getTotal(100, 1));
+// console.log(getTotal(100, 1));
 
- 
+ //Напишіть программу, яка отримує від користувача
+//число (кількість хвилин) и виводить у консоль
+//рядок у форматі годин та хвилин
+// 
+// 70 === 01:10
 
-
-
-
-
+let number = prompt('Enter number:');
+console.log(number);
+const hours = Math.floor( number / 60);
+console.log(hours);
+const minutes = number % 60;
+console.log(minutes);
+const realHours = String(hours).padStart(2, 0);
+const realMinutes = String(minutes).padStart(2, 0);
+console.log(`${realHours}:${realMinutes}`);
 
 
 

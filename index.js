fcfc0dcf81f7351cc9da633ddf04e1eaaf44e442
yuriy-aza <yuriy.aza@gmail.com>
2023-02-12 11:21:49 +0200

    task3

--- index.js
+++ index.js
@@ -16,13 +16,37 @@
 //Напишіть цикл, який виводить в консоль
 //числа от max до min по убыванию
 //Виведіть в консоль суму усіх парних чисел
-const max = 100;
-const min = 1;
-let total = 0;
-for (let i = max; i >= min; i -= 1) {
-  console.log(i);
-  if (i % 2 === 0) {
-    total += i;
-  }
+// const max = 100;
+// const min = 1;
+// let total = 0;
+// for (let i = max; i >= min; i -= 1) {
+//   console.log(i);
+//   if (i % 2 === 0) {
+//     total += i;
+//   }
+// }
+// console.log(total);
+
+function getTotal(max, min) {
+    let total = 0;
+    for (let i = max; i >= min; i -= 1) {
+        if (i % 2 !== 0) {
+            continue;
+        }
+        total += i;
+    }
+    return total;
 }
-console.log(total);
+
+console.log(getTotal(100, 1));
+
+ 
+
+
+
+
+
+
+
+
+

8a788183da459b520cfc1bc9bd878f34007ed703
aaannetty <latsdoit@gmail.com>
2023-02-12 11:08:33 +0200

    task2

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-//Використовуя функцію if...else,
-//напишіть код, який буде питати:
-//"Яка офіційна назва JavaScript?"
-//Якщо користувач вводить "ECMAScript",
-//то показати через alert: "Вірно!"
-//в противному випадку відобразити:"Не знаєте? ECMAScript!"
+// //Використовуя функцію if...else,
+// //напишіть код, який буде питати:
+// //"Яка офіційна назва JavaScript?"
+// //Якщо користувач вводить "ECMAScript",
+// //то показати через alert: "Вірно!"
+// //в противному випадку відобразити:"Не знаєте? ECMAScript!"
 
-const test = prompt('Яка офіційна назва JavaScript?');
-if(test === 'ECMAScript'){
-    alert('True')
-} else {
-    alert('False')
+// const test = prompt('Яка офіційна назва JavaScript?');
+// if(test === 'ECMAScript'){
+//     alert('True')
+// } else {
+//     alert('False')
+// }
+// console.log(test);
+
+//Напишіть цикл, який виводить в консоль
+//числа от max до min по убыванию
+//Виведіть в консоль суму усіх парних чисел
+const max = 100;
+const min = 1;
+let total = 0;
+for (let i = max; i >= min; i -= 1) {
+  console.log(i);
+  if (i % 2 === 0) {
+    total += i;
+  }
 }
-console.log(test);
+console.log(total);

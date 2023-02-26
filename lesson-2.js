3dcc74422d8e0742e4dff2517db98defbbbddfd0
UriyRatinskiy <uriy_mark@ukr.net>
2023-02-26 11:51:49 +0200

    tesk-4

--- lesson-2.js
+++ lesson-2.js
@@ -56,13 +56,13 @@
 //Функція рахує і повертає загальну вартість каменів
 //з таким ім'ям, ціною та кількістю з об'єкта
 
-const stones = [
-  { name: "Смарагд", price: 1300, quantity: 4 },
-  { name: "Діамант", price: 2700, quantity: 6 },
-  { name: "Сапфір", price: 400, quantity: 7 },
-  { name: "Щебінь", price: 150, quantity: 100 },
-  { name: "Діамант", price: 2000, quantity: 2 },
-];
+// const stones = [
+//   { name: "Смарагд", price: 1300, quantity: 4 },
+//   { name: "Діамант", price: 2700, quantity: 6 },
+//   { name: "Сапфір", price: 400, quantity: 7 },
+//   { name: "Щебінь", price: 150, quantity: 100 },
+//   { name: "Діамант", price: 2000, quantity: 2 },
+// ];
 
 // function calcTotalPrice(stones, stonesName) {
 //   let total = 0;
@@ -73,14 +73,85 @@ const stones = [
 //   }
 //   return total;
 // }
-function calcTotalPrice(stones, stonesName) {
-  return stones.reduce((total, stone) => {
-    if (stone.name === stonesName) {
-      total += stone.price * stone.quantity;
-    }
-    return total;
-  }, 0);
+// function calcTotalPrice(stones, stonesName) {
+//   return stones.reduce((total, stone) => {
+//     if (stone.name === stonesName) {
+//       total += stone.price * stone.quantity;
+//     }
+//     return total;
+//   }, 0);
 
+// }
+
+// console.log(calcTotalPrice(stones, 'Діамант'));
+
+
+//7. Напишіть скрипт керування особистим кабінетом інтернет банка
+//Є об'єкт account в якому необхідно реалізувати
+//методи для работи з балансом та історією транзакцій
+
+//Типів транзакцій всього два.
+//Можна покласти або зняти гроші з рахунка
+const Transaction = {
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw",
+};
+
+//Кожна транзакція це об'єкт з властивостями id, type, amount
+const account = {
+  //поточний баланс рахунка
+  balance: 0,
+
+  //Історія транзакцій
+  transactions: [],
+
+  //Метод створює і повертає об'єкт транзакцій
+  //Приймає сумму і тип транзакцій
+  createTransaction(type, amount) {
+    return {
+      type,
+      amount,
+    };
+  },
+
+  //Метод відповідає за додавання сумми к балансу.
+  //Приймає сумму транзакціи.
+  //Визиває createTransaction для створення об'єкта транзакціи
+  //після чого додає його в історію транзакцій
+  deposit(amount) { 
+    this.balance += amount;
+   const transaction = this.createTransaction(Transaction.DEPOSIT, amount);
+   this.transactions.push({...transaction, id: Math.random().toFixed(3)});
+  //  return this.transactions;
+   },
+
+
+  //Метод відповідає за зняття сумми з балансу.
+  //Приймає сумму транзакціи.
+  //Визиває createTransaction для створення об'єкта транзакціи
+  //після чого додає йогого в історю транзакцій
+  //Якщо amount більше ніж поточний баланс, виводимо повідомлення про те,
+  //що недостатньо коштів на рахунку
+  withdraw(amount) { 
+if (amount > this.balance){
+  return console.log(`Недостатньо коштів на рахунку`);
 }
 
-console.log(calcTotalPrice(stones, 'Діамант'));
+    this.balance -= amount;
+    const transaction = this.createTransaction(Transaction.WITHDRAW, amount);
+    this.transactions.push({...transaction, id: Math.random().toFixed(3)});
+    return this.transactions;
+   },
+
+  //Метод повертає поточний баланс
+  getBalance() {  },
+
+  //Метод шукає і повертає об'єкт транзакціи по id
+  getTransactionDetails(id) {  },
+
+//Метод повертає кількіств коштів вказаного типу
+  //транзакціи зі всієї історії транзакцій
+  getTransactionType(type) {  },
+};
+console.log(account.deposit(100));
+console.log(account.withdraw(200));

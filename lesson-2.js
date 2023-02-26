ceafa434b6d31106cce1cb0b7d8311d07a80c65b
arcane <arcanesir.neo@gmail.com>
2023-02-26 09:57:08 +0100

    task-2

--- lesson-2.js
+++ lesson-2.js
@@ -5,20 +5,47 @@
 //3 замінить значення premium на false
 //4 виводить зміст об'єкта users у форматі
 //ключ:значення використовуя Object.keys() та for...of
-const user = {
-  name: 'John',
-  age: 20,
-  hobby: 'tenis',
-  premium: true,
+// const user = {
+//   name: 'John',
+//   age: 20,
+//   hobby: 'tenis',
+//   premium: true,
+// };
+
+// user.mood = 'happy';
+// user.hobby = 'skydiving';
+// user.premium = false;
+
+// console.log(user);
+
+// const keys = Object.keys(user);
+// for (const item of keys) { 
+//   console.log(`Key - ${item}: ${user[item]}`)
+// }
+
+
+//2. У нас є об'єкт, в якому зберігаються зарплати
+//нашої команди
+//Напишіть код для додавання усіх зарплат та
+//збережіть його результат в змінній sum.
+//Якщо об'єкт salaries пустий, то результат має бути 0
+
+const salaries = {
+  Mango: 100,
+  Poly: 160,
+  Ajax: 1470,
 };
 
-user.mood = 'happy';
-user.hobby = 'skydiving';
-user.premium = false;
+const allSalaries = Object.values(salaries);
 
-console.log(user);
+// let sum = 0;
 
-const keys = Object.keys(user);
-for (const item of keys) { 
-  console.log(`Key - ${item}: ${user[item]}`)
-}
+// for(const key of allSalaries){
+//     sum += key
+
+// }
+// console.log(sum);
+
+const newSal =  allSalaries.reduce((sum, key) => sum += key, 0);
+
+console.log(newSal);

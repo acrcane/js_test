953fd7b734b1c2a50c46c743b3bcfa3feb902337
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-04-09 10:52:54 +0300

    12

---
+++ lesson-5.js
@@ -0,0 +1,58 @@
+// setTimeout(function () {
+//   console.log('A');
+//   setTimeout(function () {
+//     console.log('B');
+//     setTimeout(function () {
+//       console.log('C');
+//     }, 1000);
+//   }, 2000);
+// }, 3000);
+
+// =================================================
+
+// let i = 0;
+// setTimeout(() => alert(i), 100);
+// // setTimeout(() => alert(i), 0);
+
+// for (let j = 0; j < 1000000000; j++) {
+//   i++;
+// }
+
+// =================================================
+
+// const first = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 500, "один");
+// });
+
+// const second = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 100, "два");
+// });
+
+// Promise.race([first, second]).then((res) => console.log(res));
+
+// =================================================
+
+// setTimeout(() => console.log(1), 0);
+
+// console.log(2);
+
+// new Promise((res) => {
+//   console.log(3);
+//   res();
+// }).then(() => console.log(4));
+
+// console.log(5);
+
+// ================================================
+
+// Перероби функцію на проміс таким чином, щоб проміс повертав значення
+// через 2 секунди після виклику функції
+
+// function greet() {
+//   return "hello world";
+// }
+// const greetPromise = new Promise((resolve) => {
+//     setTimeout(() =>resolve("hello world"),
+//     2000)   
+// })
+// greetPromise.then((data) => console.log(data));

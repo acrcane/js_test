b97a1a51bd066be5c495ee2e83646eb8c7ff0730
UriyRatinskiy <uriy_mark@ukr.net>
2023-04-09 11:24:43 +0300

    task-2

--- lesson-5.js
+++ lesson-5.js
@@ -56,3 +56,25 @@
 //     2000)   
 // })
 // greetPromise.then((data) => console.log(data));
+
+
+
+// -------------------------------------------------------------------
+
+// / - Використовуй prompt та повертай значення звідти.
+// - Створи функцію, яка буде набувати значення з prompt і всередині якої буде проміс.
+// Якщо значення не є числом, відхиляй проміс та логіруй "error".
+// Якщо значення парне, вирішуй проміс та повертай "even" через 1 секунду.
+// Якщо значення не парне, вирішуй проміс та повертай "odd" через 2 секунди.
+
+// let number = prompt("Введіть число");
+// function checkNumber (number) {
+//     return new Promise((res, rej) => {
+//        number = Number(number);
+//         if(Number.isNaN(number)) rej("Error");
+//         if(number % 2 === 0) setTimeout(() =>res("even"),1000);   
+//         if(number % 2 !== 0 ) setTimeout(() =>res("odd"),2000);
+       
+//     })
+// }
+// checkNumber(number).then((data) => console.log(data)).catch((error) => console.log(error));
